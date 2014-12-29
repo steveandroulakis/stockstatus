15adf1e1cc56072d5ba969faa77403cc3263a893
Steve Androulakis <steve.androulakis@gmail.com>
2014-12-29 08:24:25 +0000

    Initial commit

---
+++ app/controllers/core.server.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+exports.index = function(req, res) {
+	res.render('index', {
+		user: req.user || null,
+		request: req
+	});
+};
+
+exports.updateStock = function() {
+  return 'HELLO';
+};
---
+++ app/models/article.server.model.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Article Schema
+ */
+var ArticleSchema = new Schema({
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	title: {
+		type: String,
+		default: '',
+		trim: true,
+		required: 'Title cannot be blank'
+	},
+	code: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	price: {
+		type: String,
+		default: 0,
+	},	
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+ArticleSchema.methods.speak = function () {
+
+  //this.update({ title: '333dd' }, function(){});
+  var stockupdater = require('../controllers/core.server.controller.js');
+  console.log(stockupdater.updateStock());
+
+  return this.title ? 'Meow name is ' + this.title : 'I don\'t have a name';
+};
+
+mongoose.model('Article', ArticleSchema);
